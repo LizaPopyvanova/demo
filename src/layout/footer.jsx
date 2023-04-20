01b8bdadb81be07ae7488e35ee7148fe74f45413
LizaPopyvanova <Lizapopyvanova@gmail.com>
2023-04-20 13:06:58 +0300

    first commit

---
+++ src/layout/footer.jsx
@@ -0,0 +1,12 @@
+function Footer(){
+    return <footer className="page-footer orange">
+
+          <div className="footer-copyright">
+            <div className="container">
+            © 2023 Liza_Popyvanova
+            <a class="grey-text text-lighten-4 right" href="#!">Repository</a>
+            </div>
+          </div>
+    </footer>
+}
+export { Footer}; 
